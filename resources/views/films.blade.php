40c6def46e4cfe3aa517919be8a79388f5086233
RobyLedda94 <robyledda01@yahoo.it>
2024-09-24 18:45:23 +0200

    [UPD] ciclate le immagini e i titoli delle carte

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -8,7 +8,18 @@
         <div class="container">
             <div class="row">
                 @foreach($movies as $movie)
-                    {{var_dump($movie)}}
+                    <div class="col-12 col-md-4 col-lg-2">
+                        <div class="carta mt-5">
+                            <img class="img-fluid" src="{{$movie['thumb']}}" alt="">
+                            <div class="corpo">
+                                <div class="titolo">
+                                    <p class="testo">
+                                        {{$movie['series']}}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             </div>
         </div>
