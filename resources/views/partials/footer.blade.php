db8ef2ffb795a78b5ccf1d10652d2517d741e192
RobyLedda94 <robyledda01@yahoo.it>
2024-09-24 20:09:02 +0200

    [UPD] inserito contenuto nella parte footer al jumbo

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -31,4 +31,112 @@
             </div>
         </div>
     </div>
+    <div class='immagine-sfondo'>
+            <div class='container'>
+                <div class='row'>
+                    <div class='col-12'>
+                        <div class='content-main-bottom d-flex justify-content-between'>
+                            <div class='left d-flex py-4'>
+                                <ul class='list-unstyled'>
+                                    <li>
+                                        <p>DC COMICS</p>
+                                    </li>
+                                    <li>
+                                        Characters
+                                    </li>
+                                    <li>
+                                        Comics
+                                    </li>
+                                    <li>
+                                        Movies
+                                    </li>
+                                    <li>
+                                        TV
+                                    </li>
+                                    <li>
+                                        Games
+                                    </li>
+                                    <li>
+                                        Videos
+                                    </li>
+                                    <li>
+                                        News
+                                    </li>
+                                    <li>
+                                        <p class='mt-3'>DC COMICS</p>
+                                    </li>
+                                    <li>
+                                        Shop DC
+                                    </li>
+                                    <li>
+                                        Shop DC Collectibles
+                                    </li>
+                                </ul>
+                                <ul class='list-unstyled mx-4'>
+                                    <li>
+                                        <p>DC</p>
+                                    </li>
+                                    <li>
+                                        Terms Of Use
+                                    </li>
+                                    <li>
+                                        Privacy policy (New)
+                                    </li>
+                                    <li>
+                                        Ad Choices
+                                    </li>
+                                    <li>
+                                        Advertising
+                                    </li>
+                                    <li>
+                                        Jobs
+                                    </li>
+                                    <li>
+                                        Subscription
+                                    </li>
+                                    <li>
+                                        Talent Workshops
+                                    </li>
+                                    <li>
+                                        CPSC Certificates
+                                    </li>
+                                    <li>
+                                        Ratings
+                                    </li>
+                                    <li>
+                                        Shop Help
+                                    </li>
+                                    <li>
+                                        Contact Us
+                                    </li>
+                                </ul>
+                                <ul class='list-unstyled'>
+                                    <li>
+                                        <p>SITES</p>
+                                    </li>
+                                    <li>
+                                        DC
+                                    </li>
+                                    <li>
+                                        MAD Magazine
+                                    </li>
+                                    <li>
+                                        DC Kids
+                                    </li>
+                                    <li>
+                                        DC Universe
+                                    </li>
+                                    <li>
+                                        DC Power Visa
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class='right'>
+                                <img class='logo-grande' src='/img/dc-logo.png'/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 </footer>
