bb9ed3eb37bee32feee5fb20bc79f547b74872dd
RobyLedda94 <robyledda01@yahoo.it>
2024-09-24 16:40:42 +0200

    [UPD] inserita le voci del menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,18 +11,19 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="content">
+                    <div class="content d-flex align-items-center justify-content-around  mt-4">
                         <img src="/img/dc-logo.png" alt="DC Comics">
-                        <ul>
-                            <li><a href=""></a>CHARACTERS</li>
-                            <li><a href=""></a>MOVIES</li>
-                            <li><a href=""></a>TV</li>
-                            <li><a href=""></a>GAMES</li>
-                            <li><a href=""></a>COLLECTIBLES</li>
-                            <li><a href=""></a>VIDEOS</li>
-                            <li><a href=""></a>FANS</li>
-                            <li><a href=""></a>NEWS</li>
-                            <li><a href=""></a>SHOP</li>
+                        <ul class="list-unstyled d-flex ">
+                            <li class="px-3"><a class="nav-link fw-bold" href="{{route('homepage')}}">CHARACTERS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold {{Route::currentRouteName() === 'homepage' ? 'active' : ''}}" href="">COMICS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">MOVIES</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">TV</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">GAMES</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">COLLECTIBLES</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">VIDEOS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">FANS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">NEWS</a></li>
+                            <li class="px-3"><a class="nav-link fw-bold" href="">SHOP</a></li>
                         </ul>
                     </div>
                 </div>
